46a6cdff05d14b52480af3d03f25c434bf0781ec
DanielModrzejewski <danio1118m@gmail.com>
2018-12-01 15:06:59 +0100

    All tests pass

--- src/main/java/com/infoshareacademy/homeworks/GradeService.java
+++ src/main/java/com/infoshareacademy/homeworks/GradeService.java
@@ -1,9 +1,11 @@
 package com.infoshareacademy.homeworks;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class GradeService {
 
@@ -12,18 +14,41 @@ public class GradeService {
             return new String[][]{{}};
         }
         else {
-            Arrays.stream(data).map(datum -> datum[0]).
+            String pattern = "#.##";
+            DecimalFormat decimalFormat = new DecimalFormat(pattern);
+            List<String> names = Arrays.stream(data).map(i -> i[0]).
                     distinct().sorted().collect(Collectors.toList());
 
-            System.out.println(Arrays.stream(data).map(datum -> datum[0]).
-                    distinct().sorted().collect(Collectors.toList()));
+            for (String[] singleData: data) {
+                System.out.println(Arrays.toString(singleData));
 
-            String pattern = "#.###";
-            DecimalFormat decimalFormat = new DecimalFormat(pattern);
+            }
 
-            String format = decimalFormat.format(123456789.123);
-            System.out.println(format);
-            return new String[][]{{"a"}};
+            String[][] result =new String [names.size()][2];
+
+            for (int i=0; i<names.size(); i++) {
+                BigDecimal counter = new BigDecimal("0");
+                BigDecimal sum = new BigDecimal("0");
+                for (String [] singleData: data) {
+                    if (names.get(i).equals(singleData[0])) {
+                        sum = sum.add(stringToBigDecimal(singleData[1]));
+                        counter = counter.add(BigDecimal.ONE);
+                    }
+                }
+                result[i][0]=names.get(i);
+                result[i][1]=String.format("%.2f",Double.valueOf(decimalFormat.format(sum.divide(counter, 2, RoundingMode.UP))));
+            }
+
+
+            return result;
+        }
+    }
+    private BigDecimal stringToBigDecimal(String value) {
+        try {
+            System.out.println(new BigDecimal(value));
+            return new BigDecimal(value);
+        } catch (Exception e) {
+            return BigDecimal.ZERO;
         }
     }
 }
